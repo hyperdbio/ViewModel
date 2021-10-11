017ad7f01aef3283b832ee267b9096a8e0120de0
Hynixys <rladbstn9408@naver.com>
2021-10-11 23:38:31 +0900

    ViewModel

---
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -0,0 +1,73 @@
+package com.example.android.unscramble.ui.game
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class GameViewModel {
+    private var _score = 0
+    val score: Int
+    get() = _score
+
+    private var _currentWordCount = 0
+    val currentWordCount : Int
+    get() = _currentWordCount
+
+    private  lateinit var _currentScrambleWord: String
+    val currentScrambleWord: String
+    get() = _currentScrambleWord
+
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
+    init {
+        Log.d("GameFragment", "GameViewModel created!")
+        getNextWord()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        tempWord.suffle()
+
+        while (tempWord.toString().equals(currentWord. false)) {
+            tempWord.shuffle()
+        }
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        }else{
+            _currentScrambleWord = String(tempWord)
+            ++_currentWordCount
+            wordsList.add(currentWord)
+        }
+    }
+
+    fun reinitializeData() {
+        _score = 0
+        _currentWordCount = 0
+        wordsList.clear()
+        getNextWord()
+    }
+
+    private fun increaseScore() {
+        _score += SCORE_INCREASE
+    }
+
+    fun isUserWordCorrect(playerWord: String): Boolean {
+        if(playerWord.equals(currentWord, true)) {
+            increaseScore()
+            return true
+        }
+        return false
+
+        fun nextWord(): Boolean {
+            return if (_currentWordCount < MAX_NO_OF_WORDS) {
+                getNextWord()
+                true
+            }else false
+        }
+    }
